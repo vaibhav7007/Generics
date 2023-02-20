d33dbb7c05e64b6875a2ba50212465e0044660a8
vaibhav <vaibhavmehta524@gmail.com>
2023-02-20 07:51:55 +0530

    Find Max int Program

---
+++ Generics/FindMaxInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class FindMaxInt
+    {
+        public static int FindIntMaxVal(int First_val, int Second_val, int Third_val)
+        {
+            if (First_val.CompareTo(Second_val) > 0 && First_val.CompareTo(Third_val) > 0)
+            {
+                Console.WriteLine("First value is Greater :" + First_val);
+                return First_val;
+            }
+            else if (Second_val.CompareTo(First_val) > 0 && Second_val.CompareTo(Third_val) > 0)
+            {
+                Console.WriteLine("Second value is Greater :" + Second_val);
+                return Second_val;
+            }
+            else
+            {
+                Console.WriteLine("Third value is Greater : " + Third_val);
+                return Third_val;
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generics programs");
+            Console.WriteLine("Choose the option from below");
+            Console.WriteLine("1.FindMaxInt");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the first value");
+                    int First_val = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the second value");
+                    int Second_val = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Three value");
+                    int Third_val = Convert.ToInt32(Console.ReadLine());
+                    FindMaxInt.FindIntMaxVal(First_val, Second_val, Third_val);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    
+    }
+}
